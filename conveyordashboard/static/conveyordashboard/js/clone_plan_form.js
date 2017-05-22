a05d086f03dc07debb9efc1a5fda884912a719bd
drngsl <drngsl@qq.com>
2017-05-22 19:55:55 +0800

    Simplify clone & migrate template
    Change-Id: Ib13c422e8fe9fa1f3ab7ab86eeec88503f34514e

---
+++ conveyordashboard/static/conveyordashboard/js/clone_plan_form.js
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2017 Huawei, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+$(function () {
+  "use strict";
+
+  if (typeof window.conveyor === "undefined") {
+    window.conveyor = {};
+  }
+
+  if (!window.conveyor.cancel_clone) {
+    window.conveyor.cancel_clone = function () {
+      var plan_id = $('#plan_topology_form').find('[name=plan_id]').val();
+      $.ajaxSetup({async: false});
+      $.ajaxSetup({beforeSend: function(xhr, settings){xhr.setRequestHeader("X-CSRFToken", $.cookie('csrftoken'));}});
+      $.post(WEBROOT + "/conveyor/plans/" + plan_id + "/cancel", function(json){});
+    }
+  }
+
+  horizon.modals.addModalInitFunction(conveyorClonePlan);
+
+  function conveyorClonePlan(modal) {
+    $(modal).find('a.cancel').click(window.conveyor.cancel_clone);
+    $(modal).find('a.close').click(window.conveyor.cancel_clone);
+  }
+
+  conveyorClonePlan($('#plan_topology_form').parent());
+});
